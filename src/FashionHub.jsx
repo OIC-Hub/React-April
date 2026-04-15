ae0f605434dbc06406b319b970794cd1cbdb4fbb
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-04-15 13:30:40 +0100

    15/04/26

---
+++ src/FashionHub.jsx
@@ -0,0 +1,15 @@
+function FashionHub() {
+  const name = "Aliyat"
+  const age = 10
+
+  return(
+    <div>
+      <p style={{fontSize: "30px", color: "green"}}>My name is {name}</p>
+      <p>I'm {age} years old</p>
+      <p>I will be {age + 1 } years old next yearr</p>
+
+    </div>
+  )
+}
+
+export default FashionHub;
